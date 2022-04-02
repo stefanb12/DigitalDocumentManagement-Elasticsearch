eda66fec067b1243f1be3b4c94f97b7c9deaa6a8
Stefan Beljic <stefan.beljic23@gmail.com>
2022-04-02 11:11:04 +0200

    Created application form

--- frontend/src/pages/ApplicationPage.js
+++ frontend/src/pages/ApplicationPage.js
@@ -1,5 +1,129 @@
-import React from "react";
+import React, { useState } from "react";
+import "../assets/css/applicationStyle.css";
 
 export default function ApplicationPage() {
-  return <div>application-page</div>;
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [degreeOfEducation, setDegreeOfEducation] = useState(1);
+
+  return (
+    <div className="container" style={{ marginLeft: 200 }}>
+      <div className="col-md-9">
+        <div className="col-md-10">
+          <div className="portlet light bordered">
+            <div className="portlet-title tabbable-line">
+              <div className="caption caption-md">
+                <i className="icon-globe theme-font hide"></i>
+                <span className="caption-subject font-blue-madison bold uppercase">
+                  Application
+                </span>
+              </div>
+            </div>
+            <div className="portlet-body">
+              <div className="tab-content">
+                <div role="tabpanel" className="tab-pane active" id="home">
+                  <div className="form-group">
+                    <label for="inputName">
+                      Name <em style={{ color: "red" }}>*</em>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputName"
+                      placeholder="Name"
+                      value={name}
+                      onChange={(event) => {
+                        setName(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputSurname">
+                      Surname <em style={{ color: "red" }}>*</em>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputSurname"
+                      placeholder="Surname"
+                      value={surname}
+                      onChange={(event) => {
+                        setSurname(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputEmail">
+                      Email <em style={{ color: "red" }}>*</em>
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="inputEmail"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(event) => {
+                        setEmail(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputAddress">
+                      Address <em style={{ color: "red" }}>*</em>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="inputAddress"
+                      placeholder="Address"
+                      value={address}
+                      onChange={(event) => {
+                        setAddress(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label for="inputDegreeOfEducation">
+                      Degree of education <em style={{ color: "red" }}>*</em>
+                    </label>
+                    <input
+                      type="number"
+                      min="1"
+                      max="8"
+                      className="form-control"
+                      id="inputDegreeOfEducation"
+                      value={degreeOfEducation}
+                      onChange={(event) => {
+                        setDegreeOfEducation(event.target.value);
+                      }}
+                    />
+                  </div>
+                  <label for="cvInputFile">
+                    CV (PDF) <em style={{ color: "red" }}>*</em>
+                  </label>
+                  <div className="form-group">
+                    <input type="file" id="cvInputFile" />
+                  </div>
+                  <label for="letterInputFile">
+                    Letter (PDF) <em style={{ color: "red" }}>*</em>
+                  </label>
+                  <div className="form-group">
+                    <input type="file" />
+                  </div>
+                  <button
+                    className="btn btn-outline-primary btn-block"
+                    style={{ marginTop: 25 }}
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/routing/Layout.js
+++ frontend/src/routing/Layout.js
@@ -24,72 +24,76 @@ export default function Layout(props) {
 
   return (
     <div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-blue" onClick={navigateToApplicationPage}>
-              <div class="inner">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-sm-6">
+            <div
+              className="card-box bg-blue"
+              onClick={navigateToApplicationPage}
+            >
+              <div className="inner">
                 <h3> Application </h3>
                 <p> Apply for a job </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+              <div className="icon">
+                <i className="fa fa-file-pdf-o" aria-hidden="true"></i>
               </div>
-              <Link to="/application" class="card-box-footer">
-                View More <i class="fa fa-arrow-circle-right"></i>
+              <Link to="/application" className="card-box-footer">
+                View More <i className="fa fa-arrow-circle-right"></i>
               </Link>
             </div>
           </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-green" onClick={navigateToSearchPage}>
-              <div class="inner">
+          <div className="col-lg-3 col-sm-6">
+            <div className="card-box bg-green" onClick={navigateToSearchPage}>
+              <div className="inner">
                 <h3> Search </h3>
                 <p> Search applicants </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-search" aria-hidden="true"></i>
+              <div className="icon">
+                <i className="fa fa-search" aria-hidden="true"></i>
               </div>
-              <Link to="/search" class="card-box-footer">
-                View More <i class="fa fa-arrow-circle-right"></i>
+              <Link to="/search" className="card-box-footer">
+                View More <i className="fa fa-arrow-circle-right"></i>
               </Link>
             </div>
           </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-orange" onClick={navigateToGeoLocationPage}>
-              <div class="inner">
+          <div className="col-lg-3 col-sm-6">
+            <div
+              className="card-box bg-orange"
+              onClick={navigateToGeoLocationPage}
+            >
+              <div className="inner">
                 <h3> Geo Location </h3>
                 <p> Search by location radius </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <div className="icon">
+                <i className="fa fa-map-marker" aria-hidden="true"></i>
               </div>
-              <Link to="/geo-location-search" class="card-box-footer">
-                View More <i class="fa fa-arrow-circle-right"></i>
+              <Link to="/geo-location-search" className="card-box-footer">
+                View More <i className="fa fa-arrow-circle-right"></i>
               </Link>
             </div>
           </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="card-box bg-red" onClick={navigateToStatisticsPage}>
-              <div class="inner">
+          <div className="col-lg-3 col-sm-6">
+            <div className="card-box bg-red" onClick={navigateToStatisticsPage}>
+              <div className="inner">
                 <h3> Statistics </h3>
                 <p> Application statistics </p>
               </div>
-              <div class="icon">
-                <i class="fa fa-line-chart"></i>
+              <div className="icon">
+                <i className="fa fa-line-chart"></i>
               </div>
-              <Link to="/statistics" class="card-box-footer">
-                View More <i class="fa fa-arrow-circle-right"></i>
+              <Link to="/statistics" className="card-box-footer">
+                View More <i className="fa fa-arrow-circle-right"></i>
               </Link>
             </div>
           </div>
         </div>
-        <div>
-          <div>{props.children}</div>
-        </div>
+        {props.children}
       </div>
-      <div class="col-md-12">
-        <div class="container bootstrap snippets bootdey">
-          <footer class="footer">
+      <div className="col-md-12">
+        <div className="container bootstrap snippets bootdey">
+          <footer className="footer">
             <hr />
             <p>&copy; UDD Elasticsearch 2022</p>
           </footer>
